b69c77cd2e5fe2b5f27b3d81d1298410481690de
Alexa Kovachevich <49769145+alexabark@users.noreply.github.com>
2020-04-23 10:41:34 -0500

    Add email table, mailing list sign up page, and a Sidekiq worker to send a welcome email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,16 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'apikey',
+  :password => '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',
+  :domain => 'dmv-running-tours.herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
+
+# 465 - for SSL
+# 25, 587 - (for unencrypted/TLS connections)
